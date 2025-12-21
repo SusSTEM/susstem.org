c80caf4af1ce67bfc1e8f6824aa7cc4516612bd1
Daivk Anand Datwani <datwani.daivik@gmail.com>
2025-12-21 12:34:05 +0400

    Revert "Update files from Figma Make"
    This reverts commit 7a5319baa9ac80397c96b314dd3ce970ac8758d2.

--- src/components/GetInvolved.tsx
+++ src/components/GetInvolved.tsx
@@ -66,7 +66,7 @@ export function GetInvolved({ onNavigate }: GetInvolvedProps) {
               <div
                 key={index}
                 className={`${
-                  isDonate ? "bg-[#072D2D]" : "bg-white hover:bg-[#a2bb65]/80 transition-colors"
+                  isDonate ? "bg-[#072D2D]" : "bg-white"
                 } rounded-3xl p-8 text-center space-y-6 shadow-md hover:shadow-xl transition-shadow flex flex-col`}
               >
                 <div
